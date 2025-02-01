f834b35dc27d83bc1e32e94f444a2cc8628fb865
DevSwayam <swayamkarle_ce_2020@ltce.in>
2025-02-02 03:35:18 +0530

    added yapster contract

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # contracts
-# contracts
-# contracts
+
+## contract address = 0xC25761bb16F45Fe7ea6df488D2A4CFc71A52b2B0
--- script/Prompt.s.sol
+++ script/Prompt.s.sol
@@ -17,5 +17,5 @@ contract PromptScript is Script {
         new Yapster(OAO_PROXY);
         vm.stopBroadcast();
     }
-    
+    // 0xC25761bb16F45Fe7ea6df488D2A4CFc71A52b2B0.
 }
